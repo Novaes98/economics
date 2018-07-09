1c7eab306a504fae21d803b36c29f23a08a85f73
arth <cupcakemayhem15@gmail.com>
2018-07-09 15:49:53 -0300

    elasticidade static

--- src/microeconomics/elasticity.java
+++ src/microeconomics/elasticity.java
@@ -2,11 +2,11 @@ package microeconomics;
 
 public class elasticity {
 	
-	public double returnPriceElasticity(int initialQtd, int finalQtd, double initialPrice, double finalPrice) {
+	public static double returnPriceElasticity(int initialQtd, int finalQtd, double initialPrice, double finalPrice) {
 		double elasticidade = (((finalQtd-initialQtd)/(finalQtd+initialQtd))/((finalPrice-initialPrice)/(finalPrice+initialPrice)))*100;
 		return elasticidade;
 	}
-	public double returnCrossElasticity(int initialQtdx, int finalQtdx, double initialPricey, double finalPricey){
+	public static double returnCrossElasticity(int initialQtdx, int finalQtdx, double initialPricey, double finalPricey){
 		double elasticidade = (((finalQtdx-initialQtdx)/(initialQtdx+finalQtdx))/((finalPricey-initialPricey)/(finalPricey+initialPricey)))*100;
 		return elasticidade;
 	}
